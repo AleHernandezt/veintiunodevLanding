873d8c0dc42d747d975b35df98cffebd6be786bd
Alehernandezt <j.alehernandezt@gmail.com>
2024-04-27 15:55:39 -0400

    font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily:{
-        'principal':['"Be Vietnam Pro"', 'sans-serif']
+        'principal':['"Montserrat", sans-serif']
       },
       colors:{
         'primary-color':["#fff6d8"],
